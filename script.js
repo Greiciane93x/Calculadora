adee26dc1559fa1d37f718322e709a9ece87013a
Greiciane93x <greiciane_araujo@hotmai.com>
2020-12-27 16:23:50 -0300

    mais algumas alteracoes no css. e correção do botão de -

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ function insere(num){
 function igual(){
     num = document.form.textoVisor.value;
     if(num){
+        // é uma função que avalia uma string e retorna um resultado; 
        document.form.textoVisor.value = eval(num);
-}
+    }
 }
 function limpaTela(){
     document.form.textoVisor.value = "";
 }
 function volta(){
     var num = document.form.textoVisor.value;
+    // substring retorna parte de uma string
+    // entre os índices inicial e final
+    // mas nesse caso retorna a string menos a última posição
     document.form.textoVisor.value = num.substring(0, num.length-1);
 }
